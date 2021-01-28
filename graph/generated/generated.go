f68df95bfbec413fbea8f4c780c7067c1ca623cc
unknown person <alex007@gmail.com>
2021-01-28 09:51:07 +0300

    pagination added

--- graph/generated/generated.go
+++ graph/generated/generated.go
@@ -59,7 +59,7 @@ type ComplexityRoot struct {
 	}
 
 	Query struct {
-		Posts func(childComplexity int) int
+		Posts func(childComplexity int, first *int, offset *int) int
 	}
 
 	Subscription struct {
@@ -71,7 +71,7 @@ type MutationResolver interface {
 	CreatePost(ctx context.Context, input model.NewPost) (*model.Post, error)
 }
 type QueryResolver interface {
-	Posts(ctx context.Context) ([]*model.Post, error)
+	Posts(ctx context.Context, first *int, offset *int) ([]*model.Post, error)
 }
 type SubscriptionResolver interface {
 	PostAdded(ctx context.Context, channName string) (<-chan *model.Post, error)
@@ -137,7 +137,12 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 			break
 		}
 
-		return e.complexity.Query.Posts(childComplexity), true
+		args, err := ec.field_Query_posts_args(context.TODO(), rawArgs)
+		if err != nil {
+			return 0, false
+		}
+
+		return e.complexity.Query.Posts(childComplexity, args["first"].(*int), args["offset"].(*int)), true
 
 	case "Subscription.PostAdded":
 		if e.complexity.Subscription.PostAdded == nil {
@@ -234,7 +239,7 @@ func (ec *executionContext) introspectType(name string) (*introspection.Type, er
 }
 
 var sources = []*ast.Source{
-	{Name: "graph/schema.graphqls", Input: `# GraphQL schema example
+	{Name: "graph/schema.graphql", Input: `# GraphQL schema example
 #
 # https://gqlgen.com/getting-started/
 
@@ -245,9 +250,7 @@ type Post{
   publicationDate: Time!
 }
 
-type Query {
-  posts: [Post!]!
-}
+
 
 input NewPost {
   title: String!
@@ -260,6 +263,10 @@ type Mutation {
 
 type Subscription {
     PostAdded(channName: String!): Post!
+}
+
+type Query {
+    posts(first: Int = 0, offset: Int = 10): [Post!]!
 }
 
 scalar Time
@@ -315,6 +322,30 @@ func (ec *executionContext) field_Query___type_args(ctx context.Context, rawArgs
 		}
 	}
 	args["name"] = arg0
+	return args, nil
+}
+
+func (ec *executionContext) field_Query_posts_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
+	var err error
+	args := map[string]interface{}{}
+	var arg0 *int
+	if tmp, ok := rawArgs["first"]; ok {
+		ctx := graphql.WithPathContext(ctx, graphql.NewPathWithField("first"))
+		arg0, err = ec.unmarshalOInt2ᚖint(ctx, tmp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	args["first"] = arg0
+	var arg1 *int
+	if tmp, ok := rawArgs["offset"]; ok {
+		ctx := graphql.WithPathContext(ctx, graphql.NewPathWithField("offset"))
+		arg1, err = ec.unmarshalOInt2ᚖint(ctx, tmp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	args["offset"] = arg1
 	return args, nil
 }
 
@@ -606,9 +637,16 @@ func (ec *executionContext) _Query_posts(ctx context.Context, field graphql.Coll
 	}
 
 	ctx = graphql.WithFieldContext(ctx, fc)
+	rawArgs := field.ArgumentMap(ec.Variables)
+	args, err := ec.field_Query_posts_args(ctx, rawArgs)
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	fc.Args = args
 	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
 		ctx = rctx // use context from middleware stack in children
-		return ec.resolvers.Query().Posts(rctx)
+		return ec.resolvers.Query().Posts(rctx, args["first"].(*int), args["offset"].(*int))
 	})
 	if err != nil {
 		ec.Error(ctx, err)
@@ -2620,6 +2658,21 @@ func (ec *executionContext) marshalOBoolean2ᚖbool(ctx context.Context, sel ast
 		return graphql.Null
 	}
 	return graphql.MarshalBoolean(*v)
+}
+
+func (ec *executionContext) unmarshalOInt2ᚖint(ctx context.Context, v interface{}) (*int, error) {
+	if v == nil {
+		return nil, nil
+	}
+	res, err := graphql.UnmarshalInt(v)
+	return &res, graphql.ErrorOnPath(ctx, err)
+}
+
+func (ec *executionContext) marshalOInt2ᚖint(ctx context.Context, sel ast.SelectionSet, v *int) graphql.Marshaler {
+	if v == nil {
+		return graphql.Null
+	}
+	return graphql.MarshalInt(*v)
 }
 
 func (ec *executionContext) unmarshalOString2string(ctx context.Context, v interface{}) (string, error) {
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -28,8 +28,24 @@ func (r *mutationResolver) CreatePost(ctx context.Context, input model.NewPost) 
 	return p, nil
 }
 
-func (r *queryResolver) Posts(ctx context.Context) ([]*model.Post, error) {
-	return r.posts, nil
+func (r *queryResolver) Posts(ctx context.Context, first *int, offset *int) ([]*model.Post, error) {
+
+	*offset += *first
+	if cap(r.posts) < *offset {
+		*offset = cap(r.posts)
+	}
+	if 0 > *offset {
+		*offset = 0
+	}
+
+	if len(r.posts) < *first {
+		*first = 0
+	}
+	if 0 > *first {
+		*first = 0
+	}
+
+	return r.posts[*first:*offset], nil
 }
 
 func (r *subscriptionResolver) PostAdded(ctx context.Context, channName string) (<-chan *model.Post, error) {
@@ -45,14 +61,7 @@ func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResol
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 // Subscription returns generated.SubscriptionResolver implementation.
-func (r *Resolver) Subscription() generated.SubscriptionResolver {
-	if r.sub == nil {
-		r.sub = make(map[string]chan *model.Post, 1)
-		postCh := make(chan *model.Post, 1)
-		r.sub["sub"] = postCh
-	}
-	return &subscriptionResolver{r}
-}
+func (r *Resolver) Subscription() generated.SubscriptionResolver { return &subscriptionResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
